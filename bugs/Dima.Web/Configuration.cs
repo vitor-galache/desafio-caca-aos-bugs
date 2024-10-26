a7f72cf0301171d3a010a18bbd417486989e5bf5
Vitor Galache Jaconely <vitorgalache5@gmail.com>
2024-10-26 20:03:34 -0300

    BUG-1 : Preparando o ambiente

--- bugs/Dima.Web/Configuration.cs
+++ bugs/Dima.Web/Configuration.cs
@@ -7,7 +7,7 @@ public static class Configuration
 {
     public const string HttpClientName = "dima";
     public static string StripePublicKey { get; set; } = "";
-    public static string BackendUrl { get; set; } = "http://localhost:5164";
+    public static string BackendUrl { get; set; } = "";
     
     public static MudTheme Theme = new()
     {
