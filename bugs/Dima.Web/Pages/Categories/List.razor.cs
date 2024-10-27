b296755efc770bba7b271c1ae93f073f0d50370d
Vitor Galache Jaconely <vitorgalache5@gmail.com>
2024-10-26 22:42:08 -0300

    BUG-3: Inicializando lista de categorias para evitar referencia nula

--- bugs/Dima.Web/Pages/Categories/List.razor.cs
+++ bugs/Dima.Web/Pages/Categories/List.razor.cs
@@ -11,7 +11,7 @@ public partial class ListCategoriesPage : ComponentBase
     #region Properties
 
     public bool IsBusy { get; set; } = false;
-    public List<Category> Categories { get; set; }
+    public List<Category> Categories { get; set; } = new();
     public string SearchTerm { get; set; } = string.Empty;
 
     #endregion
@@ -39,7 +39,7 @@ public partial class ListCategoriesPage : ComponentBase
             var request = new GetAllCategoriesRequest();
             var result = await Handler.GetAllAsync(request);
             if (result.IsSuccess)
-                Categories.AddRange(result.Data);
+                Categories.AddRange(result.Data!);
         }
         catch (Exception ex)
         {
