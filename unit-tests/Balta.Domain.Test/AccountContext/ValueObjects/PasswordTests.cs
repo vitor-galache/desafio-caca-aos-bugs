c003cc28a76541a45525a144824793b162b55977
Vitor Galache Jaconely <vitorgalache5@gmail.com>
2024-10-30 12:53:30 -0300

    Tests:PasswordTests

--- unit-tests/Balta.Domain.Test/AccountContext/ValueObjects/PasswordTests.cs
+++ unit-tests/Balta.Domain.Test/AccountContext/ValueObjects/PasswordTests.cs
@@ -1,46 +1,137 @@
+using Balta.Domain.AccountContext.ValueObjects;
+using Balta.Domain.AccountContext.ValueObjects.Exceptions;
+using Balta.Domain.Test.Mocks;
+
 namespace Balta.Domain.Test.AccountContext.ValueObjects;
 
+[Trait("ValueObject", "Password")]
 public class PasswordTests
 {
+    private const string ValidPassword = "Abcd@123";
+    private const string InvalidPassword = "1234567";
+    private const string EmptyPassword = "";
+    private const string PasswordIsWhiteSpace = " ";
     [Fact]
-    public void ShouldFailIfPasswordIsNull() => Assert.Fail();
-    
+    public void ShouldFailIfPasswordIsNull()
+    {
+        var act = () => Password.ShouldCreate(null);
+        Assert.Throws<InvalidPasswordException>(act);
+    }
+
     [Fact]
-    public void ShouldFailIfPasswordIsEmpty() => Assert.Fail();
-    
+    public void ShouldFailIfPasswordIsEmpty()
+    {
+        var act = () => Password.ShouldCreate(EmptyPassword);
+
+        Assert.Throws<InvalidPasswordException>(act);
+    }
+
     [Fact]
-    public void ShouldFailIfPasswordIsWhiteSpace() => Assert.Fail();
-    
+    public void ShouldFailIfPasswordIsWhiteSpace()
+    {
+        var act = () => Password.ShouldCreate(PasswordIsWhiteSpace);
+
+        Assert.Throws<InvalidPasswordException>(act);
+    }
+
     [Fact]
-    public void ShouldFailIfPasswordLenIsLessThanMinimumChars() => Assert.Fail();
-    
+    public void ShouldFailIfPasswordLenIsLessThanMinimumChars()
+    {
+        var act = () => Password.ShouldCreate(InvalidPassword);
+        Assert.Throws<InvalidPasswordException>(act);
+    }
+
     [Fact]
-    public void ShouldFailIfPasswordLenIsGreaterThanMaxChars() => Assert.Fail();
-    
+    public void ShouldFailIfPasswordLenIsGreaterThanMaxChars()
+    {
+        var act = () => Password.ShouldCreate(ValidPassword.PadRight(49, '0'));
+
+        Assert.Throws<InvalidPasswordException>(act);
+    }
+
     [Fact]
-    public void ShouldHashPassword() => Assert.Fail();
-    
+    public void ShouldHashPassword()
+    {
+        var password = Password.ShouldCreate(ValidPassword);
+
+        Assert.NotEmpty(password.Hash);
+    }
+
     [Fact]
-    public void ShouldVerifyPasswordHash() => Assert.Fail();
-    
+    public void ShouldVerifyPasswordHash()
+    {
+        var result = Password.ShouldCreate(ValidPassword);
+
+        var match = Password.ShouldMatch(result.Hash,ValidPassword);
+
+        Assert.True(match);
+    }
+
     [Fact]
-    public void ShouldGenerateStrongPassword() => Assert.Fail();
-    
+    public void ShouldGenerateStrongPassword()
+    {
+        bool IsSpecialCharacter(char c)
+        {
+            return !char.IsLetterOrDigit(c) && !char.IsWhiteSpace(c);
+        }
+
+        var strongPassword = Password.ShouldGenerate();
+
+        Assert.True(strongPassword.Any(char.IsNumber) && strongPassword.Any(char.IsLetter) &&
+                    strongPassword.Any(IsSpecialCharacter));
+    }
+
     [Fact]
-    public void ShouldImplicitConvertToString() => Assert.Fail();
-    
+    public void ShouldImplicitConvertToString()
+    {
+        var password = Password.ShouldCreate(ValidPassword);
+
+        Assert.Equal((string)password, password.ToString());
+    }
+
     [Fact]
-    public void ShouldReturnHashAsStringWhenCallToStringMethod() => Assert.Fail();
-    
+    public void ShouldReturnHashAsStringWhenCallToStringMethod()
+    {
+        var password = Password.ShouldCreate(ValidPassword);
+
+        Assert.Equal(password.Hash, password.ToString());
+    }
+
     [Fact]
-    public void ShouldMarkPasswordAsExpired() => Assert.Fail();
-    
+    public void ShouldMarkPasswordAsExpired()
+    {
+        var provider = new FakeDateTimeProvider();
+        var password = Password.ShouldCreate(ValidPassword,provider);
+        
+        provider.ChangeDate(DateTime.UtcNow.AddMinutes(120.1));
+        
+        Assert.True(password.IsExpired());
+    }
+
     [Fact]
-    public void ShouldFailIfPasswordIsExpired() => Assert.Fail();
-    
+    public void ShouldFailIfPasswordIsExpired()
+    {
+        var password = Password.ShouldCreate(ValidPassword);
+        
+        Assert.False(password.IsExpired());
+    }
+
     [Fact]
-    public void ShouldMarkPasswordAsMustChange() => Assert.Fail();
-    
+    public void ShouldMarkPasswordAsMustChange()
+    {
+        var provider = new FakeDateTimeProvider();
+        var password = Password.ShouldCreate(ValidPassword,provider);
+        
+        provider.ChangeDate(DateTime.UtcNow.AddMinutes(120.1));
+        
+        Assert.True(password.MustChange);
+    }
+
     [Fact]
-    public void ShouldFailIfPasswordIsMarkedAsMustChange() => Assert.Fail();
+    public void ShouldFailIfPasswordIsMarkedAsMustChange()
+    {
+        var password = Password.ShouldCreate(ValidPassword);
+        
+        Assert.False(password.MustChange);
+    } 
 }
--- unit-tests/Balta.Domain/AccountContext/ValueObjects/Password.cs
+++ unit-tests/Balta.Domain/AccountContext/ValueObjects/Password.cs
@@ -1,5 +1,7 @@
 using System.Security.Cryptography;
+using Balta.Domain.AccountContext.Providers;
 using Balta.Domain.AccountContext.ValueObjects.Exceptions;
+using Balta.Domain.SharedContext.Abstractions;
 using Balta.Domain.SharedContext.ValueObjects;
 
 namespace Balta.Domain.AccountContext.ValueObjects;
@@ -7,7 +9,8 @@ namespace Balta.Domain.AccountContext.ValueObjects;
 public record Password : ValueObject
 {
     #region Constants
-
+    
+    private const int DefaultExpirationInMinutes = 120;
     private const int MinLength = 8;
     private const int MaxLength = 48;
     private const string Valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
@@ -17,19 +20,22 @@ public record Password : ValueObject
 
     #region Constructors
 
-    private Password(string hash)
+    private Password(string hash,DateTime? expiresAtUtc,IDateTimeProvider provider)
     {
         Hash = hash;
-        ExpiresAtUtc = null;
-        MustChange = false;
+        ExpiresAtUtc = expiresAtUtc;
+        _provider = provider;
     }
 
     #endregion
 
     #region Factories
-
-    public static Password ShouldCreate(string plainText)
+    
+    public static Password ShouldCreate(string plainText) => ShouldCreate(plainText, provider:null);
+    public static Password ShouldCreate(string plainText,IDateTimeProvider? provider)
     {
+        provider = provider ?? DateTimeProvider.Default; 
+        
         if (string.IsNullOrEmpty(plainText))
             throw new InvalidPasswordException("Password cannot be null or empty");
 
@@ -44,7 +50,7 @@ public record Password : ValueObject
 
         var hash = ShouldHashPassword(plainText);
         
-        return new Password(hash);
+        return new Password(hash,expiresAtUtc: provider.UtcNow.Add(TimeSpan.FromMinutes(DefaultExpirationInMinutes)),provider:provider);
     }
 
     #endregion
@@ -53,12 +59,17 @@ public record Password : ValueObject
 
     public string Hash { get; }
     public DateTime? ExpiresAtUtc { get; }
-    public bool MustChange { get; }
+    public bool MustChange => IsExpired();
 
+    private readonly IDateTimeProvider _provider;
     #endregion
 
     #region Public Methods
 
+    public bool IsExpired()
+    {
+        return ExpiresAtUtc is null || ExpiresAtUtc < _provider.UtcNow;
+    } 
     public static string ShouldGenerate(
         short length = 16,
         bool includeSpecialChars = true,
