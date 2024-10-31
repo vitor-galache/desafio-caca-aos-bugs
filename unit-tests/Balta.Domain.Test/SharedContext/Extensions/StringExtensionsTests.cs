f37d29500916d24f951e9824552969f9f9db8d2f
Vitor Galache Jaconely <vitorgalache5@gmail.com>
2024-10-30 22:30:41 -0300

    Tests:StringExtensions

--- unit-tests/Balta.Domain.Test/SharedContext/Extensions/StringExtensionsTests.cs
+++ unit-tests/Balta.Domain.Test/SharedContext/Extensions/StringExtensionsTests.cs
@@ -4,6 +4,13 @@ namespace Balta.Domain.Test.SharedContext.Extensions;
 
 public class StringExtensionsTests
 {
-    [Fact]
-    public void ShouldGenerateBase64FromString() => Assert.Fail();
+    [Theory]
+    [InlineData("TextoTeste","VGV4dG9UZXN0ZQ==")]
+    [InlineData("","")]
+    public void ShouldGenerateBase64FromString(string text,string textInbase64)
+    {
+      var expectedBase64 = text.ToBase64();
+      
+      Assert.Equal(textInbase64, expectedBase64);
+    } 
 }
